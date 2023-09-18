f56fa2f5037354c2ccdf63cac3d29587fa443592
Sinan997 <sinanozturk997@gmail.com>
2023-09-18 20:52:32 +0300

    i dont know what i have changed, just pushing recent changes

--- src/app/pages/tables-page/tables-page.component.ts
+++ src/app/pages/tables-page/tables-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ConfirmationService, MessageService } from 'primeng/api';
+
 
 @Component({
   selector: 'app-tables-page',
   templateUrl: './tables-page.component.html',
-  styleUrls: ['./tables-page.component.scss']
+  styleUrls: ['./tables-page.component.scss'],
+  providers:[MessageService,ConfirmationService]
 })
 export class TablesPageComponent {
 
